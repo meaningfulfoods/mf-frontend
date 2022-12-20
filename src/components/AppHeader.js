aacb4dbb4456397343dc87abbbbbec7e22485f94
AMRESH <amreshaiio@gmail.com>
2022-12-20 16:07:05 +0530

    ok3

--- src/components/AppHeader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-const AppHeader = () => {
-  return (<>
-    <div style={{width:'100%',height:'4rem', backgroundColor:'grey'}}>AppHeader</div>
-  </>
-  )
-}
-
-export default AppHeader
