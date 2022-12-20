65648690783e2ce3a78338601068e7a963683921
AMRESH <amreshaiio@gmail.com>
2022-12-20 16:12:57 +0530

    ok4

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { store } from "./Redux/configureStore";
-import { Provider } from "react-redux";
 import Alert from "./assets/Alert/Alert";
 import axios from 'axios'
 
